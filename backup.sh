da3df55197a52156057ae80b42ecd72341cabc86
Carter Sprigings <carter.sprigings@mail.mcgill.ca>
2021-11-30 15:18:47 -0500

    feat: add backup script

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo yum install -y aws-cli
+
+now=$(date +"%m-%d-%Y")
+
+sudo tar -zcvf neo4j_${now}.tar.gz /var/lib/docker/volumes/neo4j
+sudo tar -zcvf "auth_${now}".tar.gz /var/lib/docker/volumes/postgres
+sudo tar -zcvf "es_${now}".tar.gz /var/lib/docker/volumes/elasticsearch
+
+aws s3 cp "neo4j_${now}".tar.gz s3://secoda-backup/
+aws s3 cp "auth_${now}".tar.gz s3://secoda-backup/
+aws s3 cp "es_${now}".tar.gz s3://secoda-backup/
+
+rm -r "auth_${now}.tar.gz" "neo4j_${now}.tar.gz" "es_${now}.tar.gz" "mysql_${now}.tar.gz"
