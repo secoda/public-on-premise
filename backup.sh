2862c7f9dbacda5d267054644f057f84db1712ee
Carter <carter.sprigings@hotmail.com>
2021-12-03 16:01:32 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,8 +8,8 @@ sudo tar -zcvf neo4j_${now}.tar.gz /var/lib/docker/volumes/neo4j
 sudo tar -zcvf "auth_${now}".tar.gz /var/lib/docker/volumes/postgres
 sudo tar -zcvf "es_${now}".tar.gz /var/lib/docker/volumes/elasticsearch
 
-aws s3 cp "neo4j_${now}".tar.gz s3://secoda-backup/
-aws s3 cp "auth_${now}".tar.gz s3://secoda-backup/
-aws s3 cp "es_${now}".tar.gz s3://secoda-backup/
+aws s3 cp "neo4j_${now}".tar.gz s3://$BUCKET_NAME/
+aws s3 cp "auth_${now}".tar.gz s3://$BUCKET_NAME/
+aws s3 cp "es_${now}".tar.gz s3://$BUCKET_NAME/
 
 rm -f "auth_${now}.tar.gz" "neo4j_${now}.tar.gz" "es_${now}.tar.gz" "mysql_${now}.tar.gz"
